e495739b95a6c148a3d778e65fd2a4a4e9e5001b
Max Oliver Geislinger <omnipotent@Maxs-MacBook-Air.local>
2017-06-25 12:32:38 -0700

    ensenmbled

--- scripts/script.py
+++ scripts/script.py
@@ -5,16 +5,18 @@ import sys
 import newspaper
 from newspaper import Article
 # from newspaper import news_pool
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.externals import joblib
-
 import re
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
+
 def clean_content ( raw_article ):
     # Function to convert a raw article to a string of words
     # The input is a single string (a raw movie article), and 
@@ -40,8 +42,10 @@ def clean_content ( raw_article ):
     # and return the result.
     return( " ".join( meaningful_words ))  
 
-def learn():
-	data = pd.read_csv("data.csv", encoding='utf-8')
+def learn(fname):
+	data = pd.read_csv(fname, encoding='utf-8')
+	# shuffle data
+	data = data.sample(frac=1).reset_index(drop=True)
 
 	for idx, row in data.iterrows():
 	    if(row["content"] is np.nan):
@@ -54,7 +58,7 @@ def learn():
 	Y = data.as_matrix()
 
 	X_train, X_test, y_train, y_test = train_test_split(
-		 Y, X, test_size=0.33, random_state=42)
+		 Y, X, test_size=0.20, random_state=42)
 
 	count_vect = CountVectorizer()
 	X_train_counts = count_vect.fit_transform(X_train[:,4])
@@ -67,20 +71,32 @@ def learn():
 	print(X_train_tfidf.shape)
 	print(X_test_tfidf.shape)
 
+
 	# add punctuation features
 	mnb_clf = MultinomialNB().fit(X_train_tfidf, y_train)
 	ada_clf = AdaBoostClassifier(n_estimators=100).fit(X_train_tfidf, y_train)
+	text_clf = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42)
+	svm_clf = text_clf.fit(X_train_tfidf, y_train)
 
 	joblib.dump(mnb_clf, 'mnb_clf.pkl')
+	joblib.dump(svm_clf, 'svm_clf.pkl')
 	joblib.dump(ada_clf, 'ada_clf.pkl')
 
 	predicted = mnb_clf.predict(X_test_tfidf)
 	ada_predictions = ada_clf.predict(X_test_tfidf)
+	svm_predictions = svm_clf.predict(X_test_tfidf)
+
 	ada_score = np.mean(ada_predictions == y_test)
 	mnb_score = np.mean(predicted == y_test)   
+	svm_score = np.mean(svm_predictions == y_test)
 
 	print(mnb_score)
 	print(ada_score)
+	print(svm_score)
+
+	sketchy_score = (mnb_score + ada_score + svm_score) / 3.0
+	print("Sketchy score")
+	print(sketchy_score)
 
 def infer(url):
 	"""
@@ -102,7 +118,7 @@ def infer(url):
 	Y = data.as_matrix()
 
 	X_train, X_test, y_train, y_test = train_test_split(
-		 Y, X, test_size=0.33, random_state=42)
+		 Y, X, test_size=0.20, random_state=42)
 
 	count_vect = CountVectorizer()
 	X_train_counts = count_vect.fit_transform(X_train[:,4])
@@ -115,6 +131,7 @@ def infer(url):
 	#######
 	a = Article(url)
 	a.download()
+	
 	X = clean_content(a.html)#.reshape(1, -1)
 	#print(X.shape) # shape is (1,1)	
 	X_counts = count_vect.transform([X])
@@ -125,16 +142,38 @@ def infer(url):
 
 	mnb_clf = joblib.load('mnb_clf.pkl')
 	ada_clf = joblib.load('ada_clf.pkl')
+	svm_clf = joblib.load('svm_clf.pkl')
 	
-	mnb_score = mnb_clf.predict_proba(X_tfidf)	
-	ada_score = ada_clf.predict_proba(X_tfidf)
+	mnb_score = mnb_clf.predict_proba(X_tfidf)[0]
+	ada_score = ada_clf.predict_proba(X_tfidf)[0]
+	svm_clf = svm_clf.predict_proba(X_tfidf)[0]
 
 	print("Adaboost score:")
 	print(ada_score)
 	print("mnb score:")
 	print(mnb_score)
+	print("svm score:")
+	print(mnb_score)
+	
+	sketchy_score = (mnb_score + ada_score + svm_score) / 3.0
+	print("Sketchy score")
+	print(sketchy_score)
 
-	return ada_score, mnb_score
+	vote = 0
+	if mnb_score > .50:
+		vote += 1
+	if ada_score > .50:
+		vote += 1
+	if svm_score > .50:
+		vote += 1
+
+
+	if vote >= 2:
+		labelPrediction = 1
+	else:
+		labelPrediction = 0
+
+	return sketchy_score, labelPrediction
 
 def main():
 	input_url = sys.argv[1]
@@ -146,4 +185,5 @@ if __name__ == "__main__":
 	# infer("http://www.theonion.com/article/scientists-confident-artificially-intelligent-mach-51170")
 
 # infer("http://www.theonion.com/graphic/sean-spicer-cradling-comfort-pig-throughout-briefi-56317")
-# learn()
+	# learn("dataset/politifact_output-cleaned.csv")
+	# learn("dataset/yo7.csv")
